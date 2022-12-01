ddecb5ec050e345922c86ffd98b23a77441a17b0
nlaureta <nlaureta@ucsc.edu>
2022-11-30 22:05:52 -0800

    main menu placeholder
    main menu

---
+++ Jam3/Scenes/MenuScene.gd
@@ -0,0 +1,18 @@
+extends Control
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$VBoxContainer/StartButton.grab_focus()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+func _on_StartButton_pressed():
+	get_tree().change_scene("res://Scenes/BaseLevel.tscn")
+
+func _on_ControlsButton_pressed():
+	pass # Replace with function body.
+
+func _on_QuitButton_pressed():
+	get_tree().quit()
